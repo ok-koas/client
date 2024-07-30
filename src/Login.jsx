3004c70b3cba402792325137edb0f4ab4f424e25
Rizal Anditama <rizalanditama2105@gmail.com>
2024-07-30 22:45:04 +0700

    added rest api to login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,23 +1,52 @@
+require("react-dotenv").config();
 import React, { useState } from "react";
+import axios from "axios"; // Import Axios
 import { FaGoogle } from "react-icons/fa"; // Import logo Google dari react-icons
-import icon from './assets/logo.png'; // Import gambar
+import icon from "./assets/logo.png"; // Import gambar
 
 const Login = () => {
+  const SERVER_LINK = process.env.SERVER_LINK;
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
   const [errors, setErrors] = useState({});
 
-  const validateForm = (event) => {
-    event.preventDefault();
+  const validateForm = () => {
     const formErrors = {};
-
-    const fields = ["username", "password"];
+    const fields = Array.from(new FormData(event.target)).map(([name]) => name);
     fields.forEach((field) => {
-      const value = event.target[field].value.trim();
-      if (!value) {
+      if (!formData[field].trim()) {
         formErrors[field] = "Field is required";
       }
     });
+    return formErrors;
+  };
 
-    setErrors(formErrors);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      setErrors({});
+      axios
+        .post(SERVER_LINK, formData)
+        .then((response) => {
+          console.log("Form submitted successfully:", response.data);
+        })
+        .catch((error) => {
+          console.error("There was an error submitting the form!", error);
+        });
+    }
   };
 
   return (
@@ -25,9 +54,10 @@ const Login = () => {
       <div className="container mx-auto px-4 py-20 mt-10">
         <div className="bg-white shadow-md rounded-lg p-8 max-w-md mx-auto">
           <div className="text-center mb-6">
-            <img src={icon} alt="Login" className="w-48 h-auto mx-auto" /> {/* Ganti teks dengan gambar */}
+            <img src={icon} alt="Login" className="w-48 h-auto mx-auto" />{" "}
+            {/* Ganti teks dengan gambar */}
           </div>
-          <form onSubmit={validateForm}>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -39,6 +69,8 @@ const Login = () => {
                 type="text"
                 name="username"
                 id="username"
+                value={formData.username}
+                onChange={handleChange}
                 className={`block px-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
                   errors.username ? "border-red-500" : ""
                 }`}
@@ -60,6 +92,8 @@ const Login = () => {
                 name="password"
                 type="password"
                 id="password"
+                value={formData.password}
+                onChange={handleChange}
                 className={`block px-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
                   errors.password ? "border-red-500" : ""
                 }`}
