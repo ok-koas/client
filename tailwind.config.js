3ff179decb7875e20fbf6a006347cad6c9ce2694
Rizal Anditama <rizalanditama2105@gmail.com>
2024-08-07 21:51:31 +0700

    editing the light theme to suit our needs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
 const { default: daisyui } = require("daisyui");
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      "seymour-one": "Seymour One",
-    },
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light"],
-  },
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			"seymour-one": "Seymour One",
+		},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			"light",
+			{
+				light: {
+					...require("daisyui/src/theming/themes").light,
+					primary: "#EC221F",
+					"primary-content": "#EFF7FF",
+					"secondary-bg": "#BFDFFE",
+					secondary: "#28666E",
+					"secondary-content": "#160C28",
+					accent: "#F4E409",
+					"accent-content": "#160C28",
+				},
+			},
+		],
+	},
 };
